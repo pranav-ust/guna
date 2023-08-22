846d023ca1257a69a97d63562f910d6577002ea2
pranav-ust <pagrawal@connect.ust.hk>
2023-08-22 17:00:14 +0800

    add

--- web.py
+++ web.py
@@ -4,8 +4,10 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('s3', type=FilesConnection)
-df = conn.read("guna-yaaasss/myfile.csv", input_format="csv", ttl=600)
 
+with conn.open("guna-yaaasss/myfile.csv", "a") as f:
+    f.write("Robert,bird")
+df = conn.read("guna-yaaasss/myfile.csv", input_format="csv", ttl=600)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.Owner} has a :{row.Pet}:")
