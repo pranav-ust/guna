54520ba7a89be96fae458e0f4b80a97f670c6e68
pranav-ust <pagrawal@connect.ust.hk>
2023-09-05 22:04:07 +0800

    added stuff

--- web.py
+++ web.py
@@ -122,6 +122,8 @@ def submit():
         for key, value in entries.items():
             f.write(json.dumps({'spanish': key, 'guna': value}) + '\n')
 
+    st.experimental_rerun()
+
 col1,col2 = st.columns(2)
 col1.title('Sum:')
 if isinstance(st.session_state.submitted, float):
